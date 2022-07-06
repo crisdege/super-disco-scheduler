d75050ab4a36d363f0cb4fbb4020fd2de128b719
Cristina Dege <cris.dege0813@gmail.com>
2022-07-06 18:49:22 -0400

    save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 var today = moment().format("dddd, MMMM Do");
-var appointment = {};
 var curentHour = moment().format("HH");
 
 document.getElementById("currentDay").innerHTML = today;
@@ -56,6 +55,118 @@ var checkFivePM = function () {
   else if (curentHour == 17) $("#fivePm").addClass("present");
   else if (curentHour < 17) $("#fivePm").addClass("future");
 };
+//load and save functions
+
+var saveNineAM = function () {
+  $(".nineAm").click(function () {
+    console.log("clicked");
+    var nineAM = document.getElementById("nineAm").value;
+    localStorage.setItem("nineAm", nineAM);
+  });
+};
+
+var loadNineAM = function () {
+  var nineAM = localStorage.getItem("nineAm");
+  console.log(nineAM);
+  $("#nineAm").text(nineAM);
+};
+
+var loadTenAM = function () {
+  var tenAM = localStorage.getItem("tenAm");
+  console.log(tenAM);
+  $("#tenAm").text(tenAM);
+};
+
+$(".tenAm").click(function () {
+  console.log("clicked");
+  var tenAM = document.getElementById("tenAm").value;
+  localStorage.setItem("tenAm", tenAM);
+});
+
+var loadElevenAM = function () {
+  var elevenAM = localStorage.getItem("elevenAm");
+  console.log(elevenAM);
+  $("#elevenAm").text(elevenAM);
+};
+
+$(".elevenAm").click(function () {
+  console.log("clicked");
+  var elevenAM = document.getElementById("elevenAm").value;
+  localStorage.setItem("elevenAm", elevenAM);
+});
+
+var loadTwelvePM = function () {
+  var twelvePM = localStorage.getItem("twelvePm");
+  console.log(twelvePM);
+  $("#twelvePm").text(twelvePM);
+};
+
+$(".twelvePm").click(function () {
+  console.log("clicked");
+  var twelvePM = document.getElementById("twelvePm").value;
+  localStorage.setItem("twelvePm", twelvePM);
+});
+
+var loadOnePM = function () {
+  var onePM = localStorage.getItem("onePm");
+  console.log(onePM);
+  $("#onePm").text(onePM);
+};
+
+$(".onePm").click(function () {
+  console.log("clicked");
+  var onePM = document.getElementById("onePm").value;
+  localStorage.setItem("onePm", onePM);
+});
+
+var loadTwoPM = function () {
+  var twoPM = localStorage.getItem("twoPm");
+  console.log(twoPM);
+  $("#twoPm").text(twoPM);
+};
+
+$(".twoPm").click(function () {
+  console.log("clicked");
+  var twoPM = document.getElementById("twoPm").value;
+  localStorage.setItem("twoPm", twoPM);
+});
+
+var loadThreePM = function () {
+  var threePM = localStorage.getItem("threePm");
+  console.log(threePM);
+  $("#threePm").text(threePM);
+};
+
+$(".threePm").click(function () {
+  console.log("clicked");
+  var threePM = document.getElementById("threePm").value;
+  localStorage.setItem("threePm", threePM);
+});
+
+var loadFourPM = function () {
+  var fourPM = localStorage.getItem("fourPm");
+  console.log(fourPM);
+  $("#fourPm").text(fourPM);
+};
+
+$(".fourPm").click(function () {
+  console.log("clicked");
+  var fourPM = document.getElementById("fourPm").value;
+  localStorage.setItem("fourPm", fourPM);
+});
+
+var loadFivePM = function () {
+  var fivePM = localStorage.getItem("fivePm");
+  console.log(fivePM);
+  $("#fivePm").text(fivePM);
+};
+
+$(".fivePm").click(function () {
+  console.log("clicked");
+  var fivePM = document.getElementById("fivePm").value;
+  localStorage.setItem("fivePm", fivePM);
+});
+
 checkNineAM();
 checkTenAM();
 checkElevenAM();
@@ -65,3 +176,14 @@ checkTwoPM();
 checkThreePM();
 checkFourPM();
 checkFivePM();
+
+saveNineAM();
+loadNineAM();
+loadTenAM();
+loadElevenAM();
+loadTwelvePM();
+loadOnePM();
+loadTwoPM();
+loadThreePM();
+loadFourPM();
+loadFivePM();
